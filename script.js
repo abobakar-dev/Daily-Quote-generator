64732ad299f4bc087f153e6ee4ae8e8c5e3e8ff1
Abobakar Sadiq <abobakarsadiq39@gmail.com>
2026-09-10 00:42:28 +0500

    style: format code for improved readability and consistency

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
-const quote =document.getElementById("quote");
-const author =document.getElementById("author");
-
-
+const quote = document.getElementById("quote");
+const author = document.getElementById("author");
+const newQuoteBtn = document.querySelector(".btn-new");
 
 const api_url = "https://dummyjson.com/quotes/random";
 async function getquote(url) {
-  const response = await fetch(url);
-  var data = await response.json();
-  console.log(data);
-  quote.innerHTML=data.quote;
-  author.innerHTML=data.author;
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      // checking the server response
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    var data = await response.json();
+
+    quote.innerHTML = data.quote;
+    author.innerHTML = data.author;
+  } catch (error) {
+    console.error("Faild to fetch quote:", error);
+    quote.innerHTML =
+      "Failed to load quote. Please check your internet connection.";
+    author.innerHTML = "Error";
+  }
 }
-getquote(api_url);
+
+window.addEventListener("DOMContentLoaded", () => {
+  getquote(api_url);
+});
+
+newQuoteBtn.addEventListener("click", () => {
+  getquote(api_url);
+});
